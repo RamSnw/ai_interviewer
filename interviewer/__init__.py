f209283ae403ea06963f60dd153ca0587e68facf
RMicle <raluca.micle@gmail.com>
2026-04-07 22:11:26 +0300

    Initialize interviewer package
    Added __init__.py to mark the interviewer folder as a Python package.
    
    This allows modules inside the folder to be imported using clean import statements, such as:
    from interviewer.job_loader import load_job_descriptions
    
    The file does not contain logic, but helps define the project structure.

---
+++ interviewer/__init__.py
@@ -0,0 +1,10 @@
+# This file can stay empty.
+#
+# This file marks the "interviewer" folder as a Python package.
+# That allows other files in the project to import modules from this folder
+# using clean import statements such as:
+# from interviewer.job_loader import load_job_descriptions
+#
+# The file does not need to contain any code.
+# In this project, its main purpose is to make the package structure clear
+# and keep imports simple and organized.
